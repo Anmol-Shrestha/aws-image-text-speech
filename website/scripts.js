d1a5f2e208b9fde0173886032007d328c411813c
anmol_sstha <anmolstha777@gmail.com>
2026-05-03 23:13:09 -0400

    Fix double slash in API URL
    - Remove trailing slash from serverUrl to prevent //images
    - URL is now: /api/images (not /api//images)
    - Should resolve 500 error on upload

--- website/scripts.js
+++ website/scripts.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-const serverUrl = "https://v9c90wf7fj.execute-api.us-east-1.amazonaws.com/api/";
+const serverUrl = "https://v9c90wf7fj.execute-api.us-east-1.amazonaws.com/api";
 
 async function uploadImage() {
     // encode input file as base64 string for upload
